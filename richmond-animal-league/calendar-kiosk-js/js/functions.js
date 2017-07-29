c691df78aac62b18a4ea0676d832c3435006522f
Brandon Feldkamp <brandon.feldkamp@capitalone.com>
2017-07-29 14:52:29 -0400

    move inline js to script, add next,prev buttons

---
+++ richmond-animal-league/calendar-kiosk-js/js/functions.js
@@ -0,0 +1,38 @@
+var buttonClickCount = 0;
+var currentlySelectedDate = new Date();
+
+function addClickHandlers(){
+  $("#nextDayButton, #prevDayButton").click(function(event){
+    event.preventDefault();
+
+    if(this.id === 'nextDayButton'){
+      buttonClickCount++;
+    } else if(this.id === 'prevDayButton'){
+      buttonClickCount--;
+    }
+
+    setTimeout(function(){
+      //update global date
+      currentlySelectedDate.setDate(currentlySelectedDate.getDate() + buttonClickCount);
+
+      //reset global count
+      buttonClickCount = 0;
+
+      //update date header
+      updateDateHeader(currentlySelectedDate);
+    }, 1000);
+  });
+}
+
+function updateDateHeader(selectedDate){
+  if(!selectedDate){
+    selectedDate = new Date();
+  }
+
+  $("#today").text(selectedDate.toLocaleString('en-us', {  weekday: 'long', month: 'long', day:'2-digit' }));
+}
+
+$(document).ready(function() {
+    updateDateHeader();
+    addClickHandlers();
+});
