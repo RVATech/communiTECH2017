9949be6039e6021e513a88bfc58f1faf91949a6e
ts <ts>
2017-07-29 10:09:50 -0400

    Updated setup files for connecting to Docker version of MySQL.  Additionally created connection code

---
+++ src/db/connect_to_db.js
@@ -0,0 +1,23 @@
+//https://hackernoon.com/nodejs-mysql-install-setup-tutorial-example-connection-insert-query-fd5c5032d3b0
+var mysql = require("mysql");
+// First you need to create a connection to the db
+var con = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "mysqlpw",
+    database: ""
+});
+con.connect(function(err){
+    if(err){
+        console.log('Error connecting to Db');
+        return;
+    }
+    console.log('Connection established');
+});
+con.end(function(err) {
+    // The connection is terminated gracefully
+    // Ensures all previously enqueued queries are still
+    // before sending a COM_QUIT packet to the MySQL server.
+    if(err) console.log('err: ', err);
+    else console.log('Terminated done: ');
+});
